72f13c0c4aba84ecd96cab2d71ebb25169c2d4c8
Gideon VanRiette <gideon@postscript.io>
2021-07-15 16:45:06 -0500

    Create a model for the database

--- courier/__init__.py
+++ courier/__init__.py
@@ -1,8 +1,10 @@
 """Top-level package for Courier."""
 
 __author__ = """Gideon VanRiette"""
-__email__ = 'gideon@postscript.io'
-__version__ = '0.1.0'
+__email__ = "gideon@postscript.io"
+__version__ = "0.1.0"
 
 
 from . import settings
+from . import models
+from . import database
---
+++ courier/models/__init__.py
@@ -0,0 +1,26 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy import TEXT, Column, DateTime
+from sqlalchemy.dialects import postgresql
+
+from ..database import Base
+
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id: uuid.UUID = Column(
+        postgresql.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    type: str = Column(TEXT, nullable=False)
+    body = Column(postgresql.JSONB, nullable=False)
+    destination: str = Column(TEXT, nullable=False)
+    deliver_after: str = Column(DateTime, default=datetime.utcnow, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(
+        DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+        nullable=False,
+    )
